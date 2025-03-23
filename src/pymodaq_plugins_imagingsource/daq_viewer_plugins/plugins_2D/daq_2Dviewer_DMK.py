c8d8dc41f847273a23d22a9bc8dc5ab275d47232
Christian Cabello <christian.cabello@ip-paris.fr>
2025-03-23 19:00:10 +0100

    Working implementation ; needs fine tuning (laggy)

--- src/pymodaq_plugins_imagingsource/daq_viewer_plugins/plugins_2D/daq_2Dviewer_DMK.py
+++ src/pymodaq_plugins_imagingsource/daq_viewer_plugins/plugins_2D/daq_2Dviewer_DMK.py
@@ -1,53 +1,13 @@
 import numpy as np
+import time
 from pymodaq.utils.daq_utils import ThreadCommand
 from pymodaq.utils.data import DataFromPlugins, Axis, DataToExport
 from pymodaq.control_modules.viewer_utility_classes import DAQ_Viewer_base, comon_parameters, main
 from pymodaq.utils.parameter import Parameter
 from PyQt5.QtCore import pyqtSignal
+from pymodaq_plugins_imagingsource.hardware.ImagingSource import ic4, Listener, ImagingSourceCamera
 
 from qtpy import QtWidgets, QtCore
-from time import perf_counter
-
-import imagingcontrol4 as ic4
-import cv2
-
-ic4.Library.init()
-
-class ProcessAndDisplayListener(ic4.QueueSinkListener):
-    # Listener to demonstrate processing and displaying received images
-
-    def __init__(self, d: ic4.Display):
-        self.display = d
-
-    def sink_connected(self, sink: ic4.QueueSink, image_type: ic4.ImageType, min_buffers_required: int) -> bool:
-        # Just accept whatever is passed
-        return True
-
-    def frames_queued(self, sink: ic4.QueueSink):
-        # Get the new buffer from the sink
-        buffer = sink.pop_output_buffer()
-
-        # Create a numpy view onto the buffer
-        # This view is only valid while the buffer itself exists,
-        # which is guaranteed by them both not being passed out of this function
-        buffer_wrap = buffer.numpy_wrap()
-
-        # Blur the buffer in-place using a rather large kernel
-        cv2.blur(buffer_wrap, (31, 31), buffer_wrap)
-
-        # Write some text so that the user doesn't hopelessly try to focus the lens
-        cv2.putText(
-            buffer_wrap,
-            "This image is blurred using OpenCV",
-            (100, 100),
-            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-            fontScale=1,
-            color=(255, 0, 0),
-            thickness=2,
-        )
-
-        # Send the modified buffer to the display
-        self.display.display_buffer(buffer)
 
 
 
@@ -81,11 +41,19 @@ class DAQ_2DViewer_DMK(DAQ_Viewer_base):
 
     """
     params = params = comon_parameters + [
-        {'title': 'Camera model:', 'name': 'camera_name', 'type': 'str', 'value': '', 'readonly': True},
-        {'title': 'Image width', 'name': 'width', 'type': 'int', 'value': 1280, 'default': 1280, 'min': 640, 'max': 1280},
-        {'title': 'Image height', 'name': 'height', 'type': 'int', 'value': 960, 'default': 960, 'min': 480, 'max': 960},
-        {'title': 'Exposure time (ms)', 'name': 'exposure_time', 'type': 'float', 'value': 100.0, 'default': 100.0, 'min': 100.0, 'max': 30000000.0},
-        {'title': 'Gain', 'name': 'gain', 'type': 'float', 'value': 34, 'default': 34, 'min': 34, 'max': 255}
+        {'title': 'Camera Model:', 'name': 'camera_name', 'type': 'str', 'value': '', 'readonly': True},
+        {'title': 'Image Width', 'name': 'width', 'type': 'int', 'value': 1280, 'default': 1280, 'min': 96, 'max': 1280},
+        {'title': 'Image Height', 'name': 'height', 'type': 'int', 'value': 960, 'default': 960, 'min': 96, 'max': 960},
+        {'title': 'Exposure', 'name': 'exposure', 'type': 'group', 'children': [
+            {'title': 'Auto Exposure', 'name': 'exposure_auto', 'type': 'list', 'value': "Off", 'limits': ['On', 'Off']},
+            {'title': 'Exposure Time (ms)', 'name': 'exposure_time', 'type': 'float', 'value': 100.0, 'default': 100.0, 'min': 100.0, 'max': 30000000.0}
+        ]},
+        {'title': 'Gain', 'name': 'gain', 'type': 'group', 'children': [
+            {'title': 'Auto Gain', 'name': 'gain_auto', 'type': 'list', 'value': "Off", 'limits': ['On', 'Off']},
+            {'title': 'Value', 'name': 'gain_value', 'type': 'float', 'value': 34, 'default': 34, 'min': 34, 'max': 255}
+        ]},
+        {'title': 'Frame Rate', 'name': 'frame_rate', 'type': 'float', 'value': 25, 'default': 25, 'min': 7.5, 'max': 25},
+        {'title': 'Gamma', 'name': 'gamma', 'type': 'float', 'value': 1, 'default': 1, 'min': 1, 'max': 500}
         
     ]
 
@@ -95,22 +63,18 @@ class DAQ_2DViewer_DMK(DAQ_Viewer_base):
         """Initialize attributes"""
 
         self.controller: ic4.Grabber = None
-        self.model_name = ic4.DeviceEnum.devices()[0].model_name
+        self.device_info = None
         self.map = None
-        self.sink = ic4.QueueSink([ic4.PixelFormat.BGR8], max_output_buffers=1)
+        self.listener = None
+        self.sink = None
 
-        
-        self.width = 1
-        self.height = 1
-        self.exposure_time = 1
-        self.gain = 1
-
-
-        self.x_axis = Axis(data=np.linspace(0, 1, self.width))
-        self.y_axis = Axis(data=np.linspace(0, 1, self.height))
+        self.x_axis = None
+        self.y_axis = None
 
         self.data_shape = 'Data2D'
         self.callback_thread = None
+        self.last_read_frame = None
+        self.last_wait_frame = None
 
     def commit_settings(self, param: Parameter):
         """Apply the consequences of a change of value in the detector settings
@@ -124,12 +88,19 @@ class DAQ_2DViewer_DMK(DAQ_Viewer_base):
             self.controller.device_property_map.set_value(ic4.PropId.WIDTH, param.value)
         elif param.name() == "height":
             self.controller.device_property_map.set_value(ic4.PropId.HEIGHT, param.value)
+        elif param.name() == "auto_exposure":
+            self.controller.device_property_map.set_value('Exposure_Auto', param.value)
         elif param.name() == "exposure_time":
-            self.controller.device_property_map.set_value(ic4.PropId.EXPOSURE, param.value)
+            self.controller.device_property_map.set_value(ic4.PropId.EXPOSURE_TIME, param.value)
+        elif param.name() == "auto_gain":
+            self.controller.device_property_map.set_value('Gain_Auto', param.value)
         elif param.name() == "gain":
             self.controller.device_property_map.set_value(ic4.PropId.GAIN, param.value)
+        elif param.name() == "frame_rate":
+            self.controller.device_property_map.set_value(ic4.PropId.ACQUISITION_FRAME_RATE, param.value)
+        elif param.name() == "gamma":
+            self.controller.device_property_map.set_value(ic4.PropId.GAMMA, param.value)
 
-        
 
     def ini_detector(self, controller=None):
         """Detector communication initialization
@@ -146,67 +117,57 @@ class DAQ_2DViewer_DMK(DAQ_Viewer_base):
         initialized: bool
             False if initialization failed otherwise True
         """
-        
-        if not ic4.DeviceEnum.devices():
-            raise Exception("No Imaging Source camera found")
+        ic4.Library.init(api_log_level=ic4.LogLevel.INFO, log_targets=ic4.LogTarget.STDERR)
+
 
         self.ini_detector_init(old_controller=controller,
                                new_controller=ic4.Grabber())
         
+        self.device_info = ic4.DeviceEnum.devices()[0]
         self.controller.device_open(self.device_info)
-        self.controller.stream_setup(self.sink, ic4.StreamSetupOption.DEFER_ACQUISITION_START)
+        self.settings.param('camera_name').setValue(self.device_info.model_name)
         self.map = self.controller.device_property_map
+        self.controller.device_property_map.try_set_value(ic4.PropId.PIXEL_FORMAT, ic4.PixelFormat.Mono8)
 
-        self.map.set_value(ic4.PropId.WIDTH, self.width)
-        self.map.set_value(ic4.PropId.HEIGHT, self.height)
-        self.map.set_value(ic4.PropId.EXPOSURE_TIME, self.exposure_time)
-        self.map.set_value(ic4.PropId.GAIN, self.gain)
+        self.settings.param('width').setValue(self.map.get_value_int(ic4.PropId.WIDTH))
+        self.settings.param('height').setValue(self.map.get_value_int(ic4.PropId.HEIGHT))
+        self.settings.child('exposure', 'exposure_auto').setValue(self.map.get_value_bool('Exposure_Auto'))
+        self.settings.child('exposure', 'exposure_time').setValue(self.map.get_value_float(ic4.PropId.EXPOSURE_TIME))
+        self.settings.child('gain', 'gain_auto').setValue(self.map.get_value_bool('Gain_Auto'))
+        self.settings.child('gain', 'gain_value').setValue(self.map.get_value_float(ic4.PropId.GAIN))
+        self.settings.param('frame_rate').setValue(self.map.get_value_float(ic4.PropId.ACQUISITION_FRAME_RATE))
+        self.settings.param('gamma').setValue(self.map.get_value_float(ic4.PropId.GAMMA))
 
-        self.x_axis = Axis(data=np.linspace(1, self.map[ic4.PropId.WIDTH].maximum, self.width))
-        self.y_axis = Axis(data=np.linspace(1, self.map[ic4.PropId.HEIGHT].maximum, self.height))
+        self.x_axis = Axis(data=np.linspace(1, self.map[ic4.PropId.WIDTH].maximum, self.map.get_value_int(ic4.PropId.WIDTH)))
+        self.y_axis = Axis(data=np.linspace(1, self.map[ic4.PropId.HEIGHT].maximum, self.map.get_value_int(ic4.PropId.HEIGHT)))
 
-        callback = ImagingSourceCallback(self.grabber, self.sink)
+        # Way to define a wait function with arguments
+        #wait_func = lambda: self.wait_for_frame(since='lastread', timeout=20.0)
+        #callback = ImagingSourceCallback(wait_func)
 
-        self.callback_thread = QtCore.QThread()  # Create a separate Qt thread
-        callback.moveToThread(self.callback_thread)  # Move callback object to thread
-
-        callback.data_sig.connect(self.emit_data)  # When data is available, process it
-
-        self.callback_signal.connect(callback.wait_for_acquisition)  # Connect signal to wait function
-        self.callback_thread.callback = callback
-        self.callback_thread.start()
+        #self.callback_thread = QtCore.QThread()  # creation of a Qt5 thread
+        #callback.moveToThread(self.callback_thread)  # callback object will live within this thread
+        #callback.data_sig.connect(self.emit_data)  # when the wait for acquisition returns (with data taken), emit_data will be fired
+        #self.callback_signal.connect(callback.wait_for_acquisition)
+        #self.callback_thread.callback = callback
+        #self.callback_thread.start()
 
         info = "Imaging Source camera initialized"
+        print("Imaging Source camera initialized successfully")
         initialized = True
         return info, initialized
 
     def close(self):
         """Terminate the communication protocol"""
+        if self.controller.is_streaming:
+            self.controller.stream_stop()
         self.controller.device_close()
+        ic4.Library.exit()
         self.controller = None  # Garbage collect the controller
         self.status.initialized = False
         self.status.controller = None
         self.status.info = ""
-
-    def _prepare_view(self):
-        """Preparing a data viewer by emitting temporary data. Typically, needs to be called whenever the
-        ROIs are changed"""
-
-        mock_data = np.zeros((self.width, self.height))
-
-        if self.width != 1 and self.height != 1:
-            data_shape = 'Data2D'
-        else:
-            data_shape = 'Data1D'
-
-        if data_shape != self.data_shape:
-            self.data_shape = data_shape
-            # init the viewers
-            self.data_grabed_signal_temp.emit([DataFromPlugins(name='Thorlabs Camera',
-                                                               data=[np.squeeze(mock_data)],
-                                                               dim=self.data_shape,
-                                                               labels=[f'ThorCam_{self.data_shape}'])])
-            QtWidgets.QApplication.processEvents()    
+        print(f"Camera communication terminated successfully")   
 
     def grab_data(self, Naverage=1, **kwargs):
         """
@@ -215,17 +176,14 @@ class DAQ_2DViewer_DMK(DAQ_Viewer_base):
         Naverage: (int) Number of averaging
         kwargs: (dict) of others optionals arguments
         """
-        try:
-            # Warning, acquisition_in_progress returns 1,0 and not a real bool
-            if not self.controller.is_acquisition_active:
-                self.controller.acquisition_stop()
-                self.controller.acquisition_start()
+    
+        self.sink = ic4.SnapSink()
+        self.sink.AllocationStrategy(10, 4, 6, 0)
+        self.controller.stream_setup(self.sink, setup_option=ic4.StreamSetupOption.ACQUISITION_START)
 
-            # Then start the acquisition
-            self.callback_signal.emit()  # will trigger the wait for acquisition
-
-        except Exception as e:
-            self.emit_status(ThreadCommand('Update_Status', [str(e), "log"]))
+        #self.callback_signal.emit()  # will trigger the wait for acquisition
+        self.emit_data()
+        self.controller.stream_stop()
 
     def emit_data(self):
         """
@@ -235,12 +193,12 @@ class DAQ_2DViewer_DMK(DAQ_Viewer_base):
             daq_utils.ThreadCommand
         """
         try:
-            # Get  data from buffer
-            frame = self.sink.pop_output_buffer()
-            # Emit the frame.
-            if frame is not None:  # happens for last frame when stopping camera
+            buffer = self.sink.snap_single(1000)
+            image = buffer.numpy_copy()
+            buffer.release()
+            if image is not None:
                 self.data_grabed_signal.emit([DataFromPlugins(name='DMK Camera',
-                                                              data=[np.squeeze(frame)],
+                                                              data=[np.squeeze(image)],
                                                               dim=self.data_shape,
                                                               labels=[f'DMK_{self.data_shape}'])])
 
@@ -257,6 +215,7 @@ class DAQ_2DViewer_DMK(DAQ_Viewer_base):
     def device_lost(self):
         token = self.controller.event_add_device_lost(self.handle_device_lost(self.controller))
         self.controller.event_remove_device_lost(token)
+        self.controller.stream_stop()
         self.controller.device_close()
 
 
@@ -270,27 +229,77 @@ class DAQ_2DViewer_DMK(DAQ_Viewer_base):
                                                                 y_axis=self.y_axis), ]))
     def stop(self):
         """Stop the current grab hardware wise if necessary"""
-        self.controller.acquisition_stop()  # when writing your own plugin replace this line
-        self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
+
         return ''
     
-class ImagingSourceCallback(QtCore.QObject):
-    """Callback object for Imaging Source camera"""
-    data_sig = QtCore.Signal()
+    def wait_for_frame(self, since="lastread", timeout=20.0, period=1E-3):
+        """
+        Wait for a new camera frame.
 
-    def __init__(self, grabber, sink):
+        Parameters:
+        - `since`: Defines the reference point for new frames. Can be:
+            - `"lastread"`: Wait for a frame after the last read frame.
+            - `"lastwait"`: Wait for a frame after the last `wait_for_frame` call.
+            - `"now"`: Wait for a frame acquired after this function call.
+        - `timeout`: Maximum waiting time (in seconds).
+        - `period`: Polling interval (in seconds).
+
+        Raises:
+        - `TimeoutError`: If no frame is received within `timeout`.
+        """
+        start_time = time.time()
+
+        if since not in {"lastread", "lastwait", "now"}:
+            raise ValueError(f"Invalid 'since' value: {since}")
+
+        if since == "lastwait":
+            while time.time() - start_time < timeout:
+                try:
+                    image = self.sink.snap_single(1000)
+                    if image is not None:
+                        self.last_wait_frame = image
+                        return image
+                except ic4.IC4Exception:
+                    pass
+                time.sleep(period)
+            raise TimeoutError("⏳ Frame acquisition timed out!")
+
+        elif since == "lastread":
+            while self.get_new_images_range() is None:
+                self.wait_for_frame(since="lastwait", timeout=timeout)
+
+        else:  # "now"
+            last_img = self.get_new_images_range()
+            while True:
+                self.wait_for_frame(since="lastwait", timeout=timeout)
+                new_img = self.get_new_images_range()
+                if new_img and (last_img is None or new_img[1] > last_img):
+                    return
+
+    def get_new_images_range(self):
+        """Returns the index of the latest frame (if any)."""
+        try:
+            image = self.sink.snap_single(1000)
+            if image is not None:
+                self.last_read_frame = image
+                return (self.last_read_frame, self.last_read_frame)  # Simulating a range
+        except ic4.IC4Exception:
+            pass
+        return None
+    
+class ImagingSourceCallback(QtCore.QObject):
+    """Callback object """
+    data_sig = pyqtSignal()
+
+    def __init__(self, wait_fn):
         super().__init__()
-        self.grabber = grabber  # Reference to the Grabber
-        self.sink = sink  # Reference to the SnapSink
+        # Set the wait function
+        self.wait_fn = wait_fn
 
     def wait_for_acquisition(self):
-        """Waits for the next frame and emits signal when ready"""
-        try:
-            image = self.sink.snap_single(1000)  # Wait up to 1000ms for a frame
-            if image is not None:
-                self.data_sig.emit()  # Notify that a new frame is available
-        except ic4.IC4Exception as ex:
-            print(f"Error in acquisition: {ex.message}")
+        new_data = self.wait_fn()
+        if new_data is not False:  # will be returned if the main thread called CancelWait
+            self.data_sig.emit()
 
 
 if __name__ == '__main__':
