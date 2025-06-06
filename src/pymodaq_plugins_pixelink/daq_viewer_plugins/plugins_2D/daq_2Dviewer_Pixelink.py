5c825a60f4e60e9cce3bef996bcbad0fe04479d0
Christian Cabello <christian.cabello@ip-paris.fr>
2025-06-06 15:43:37 +0200

    Only emit metadata when in trigger saving mode

--- src/pymodaq_plugins_pixelink/daq_viewer_plugins/plugins_2D/daq_2Dviewer_Pixelink.py
+++ src/pymodaq_plugins_pixelink/daq_viewer_plugins/plugins_2D/daq_2Dviewer_Pixelink.py
@@ -441,7 +441,7 @@ class DAQ_2DViewer_Pixelink(DAQ_Viewer_base):
                 iio.imwrite(filepath, frame)
 
         # Finally, handle publishing with LECO, including frame raw data if enabled to log frame captured/saved event
-        if self.data_publisher is not None:
+        if self.data_publisher is not None and self.save_frame:
             if self.send_frame_leco:                        
                 self.data_publisher.send_data2({self.settings.child('leco_log', 'publisher_name').value(): 
                                                 {'frame': frame, 'metadata': metadata, 
